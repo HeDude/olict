823ce9454afdd319a158dd73a0d836f92e80b6f4
bijbrengen <slagter@bijbrengen.nl>
2019-10-29 14:18:48 +0100

    Starting as fork of codeniacs

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta http-equiv="refresh" content="5;url=https://www.codeniacs.nl">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Codeniacs">
+    <meta name="copyright" content="Codeniacs">
+    <title>Ondernemend Leren door ICT</title>
+
+    <link rel="shortcut icon" href="images/favicon.ico">
+</head>
+
+<body>
+    <section>
+            <h2 style="text-align: center">Elict is een fork van <a href="https://www.codeniacs.nl">Codeniacs<a/></h2>
+            <p style="text-align: center">Elict zal starten in schooljaar 2020-2021</p>
+    </section>
+</body>
+</html>
